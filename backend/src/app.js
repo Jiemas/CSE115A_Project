d582df9edff8e442817872ffb9f49d32e9eafbca
Jiemas <jiemasrubio@yahoo.com>
2024-10-11 20:14:05 -0700

    Removed extra modules, need to do this in main too

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const express = require('express');
-const cors = require('cors');
 const yaml = require('js-yaml');
 const swaggerUi = require('swagger-ui-express');
 const fs = require('fs');
@@ -10,7 +9,6 @@ const OpenApiValidator = require('express-openapi-validator');
 const set = require('./set')
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 

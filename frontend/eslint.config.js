2eb3f48a43eb6ebd0ee52a84b1bfe2f3679fb630
Jianwen <jwu287@ucsc.edu>
2024-10-26 11:30:25 -0700

    adding eslint config file

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,32 +1,31 @@
-module.exports = {
-    env: {
-      browser: true,
-      es2021: true
+const js = require('@eslint/js');
+const reactPlugin = require('eslint-plugin-react');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const prettierPlugin = require('eslint-plugin-prettier');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      react: reactPlugin,
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
     },
-    extends: [
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "plugin:react/jsx-runtime",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:prettier/recommended"
-    ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true
+    languageOptions: {
+      parser: tsPlugin.parser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
-      ecmaVersion: 2021,
-      sourceType: "module"
     },
-    plugins: [
-      "react",
-      "react-hooks",
-      "@typescript-eslint",
-      "prettier"
-    ],
     rules: {
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      "prettier/prettier": "warn"
-    }
-  };
+      'prettier/prettier': 'error',
+      // Add your custom rules here
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+];

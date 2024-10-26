6333850561e89d900d93759ce2e65dcd0ede1c8a
Jianwen <jwu287@ucsc.edu>
2024-10-26 12:02:57 -0700

    Updated the config file with react, ts, and pretty recommended plugins

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,9 +2,19 @@ const js = require('@eslint/js');
 const reactPlugin = require('eslint-plugin-react');
 const tsPlugin = require('@typescript-eslint/eslint-plugin');
 const prettierPlugin = require('eslint-plugin-prettier');
+const { FlatCompat } = require('@eslint/eslintrc');
+
+const compat = new FlatCompat();
 
 module.exports = [
   js.configs.recommended,
+  ...compat.config({
+    extends: [
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended'
+    ]
+  }),
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     plugins: {

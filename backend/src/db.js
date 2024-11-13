bb7974c21be8169198301c39986ede013aedde58
Tiffany Guan <tiguan2244@gmail.com>
2024-11-12 17:09:49 -0800

    adjusted test LLM prompt to return JSON obj only, added test DB functions to add wrong and correct answers generated from test LLM prompt

--- backend/src/db.js
+++ backend/src/db.js
@@ -124,39 +124,116 @@ exports.deleteUser = async (key) => {
   await fetch(`${rootPath}/user/${key}.json`, {method: 'DELETE'});
 };
 
-// ------------ tiff edits ----------------------------------
-// db function to store/access the wrong answers?
-// exports.wrongAnswers = async (setId, cardId, parsedObj) => {
+// ---------------- tiff edits ---------------------------------
 
-// };
-
-// db function to store/access the right answers?
-// exports.rightAnswers = async (setId, cardId, parsedObj) => {
-
-// };
-
-// function to grab Set details (params/body)
-exports.setDetails = async (setId) => {
-  const answer = await fetch(`${rootPath}/set/${setId}.json`,
-    {method: 'GET'},
-  );
-  const set = await answer.json();
-  return set ? set : null;
+// For LLM Test Function:
+// Currently setting the obj label to be the front of test card
+// FUTURE: path should be wrong_answers/setId/cardId etc.
+exports.addWrongAnswers = async (front, wrongAnswersList) => {
+  const wrongAnswersObj = {wrong_answers: wrongAnswersList};
+  await fetch(`${rootPath}/Test_LLM_responses/${front}.json`, {
+    method: 'PATCH',
+    body: JSON.stringify(wrongAnswersObj),
+    headers: {'Content-Type': 'application/json'},
+  });
 };
+
+exports.getWrongAnswers = async () => {
+  const answer = await fetch(`${rootPath}/wrong_answers.json`, {
+    method: 'GET',
+  });
+  const correctAnswers = await answer.json();
+  return correctAnswers ? correctAnswers : [];
+};
+
+exports.addCorrectAnswers = async (front, correctAnswersList) => {
+  const correctAnswersObj = {correct_answers: correctAnswersList};
+  await fetch(`${rootPath}/Test_LLM_responses/${front}.json`, {
+    method: 'PATCH',
+    body: JSON.stringify(correctAnswersObj),
+    headers: {'Content-Type': 'application/json'},
+  });
+};
+
+// -----------------------------------------------------------
+
+// Theoretically, these functions should create a sub section under
+// card -> set key -> card key so we should be able to see the stored
+// answers like this:
+//
+// card ->set_key -> card_key -> wrong_answers
+// and
+// card ->set_key -> card_key -> correct_answers
+//
+// But idk where we want the data stored yet :'D so I have not
+// ran the code bc I don't want to break the DB :p
+// ----------------------------------------------------------------
+
+// DB functions to access and add wrong/correct answers to cards
+// exports.getWrongAnswers = async (setId, cardId) => {
+//   const answer = await fetch(`${rootPath}/card/${setId}/${cardId}
+//     /wrong_answers.json`, {
+//     method: 'GET',
+//   });
+//   const wrongAnswers = await answer.json();
+//   return wrongAnswers ? wrongAnswers : [];
+// };
+
+// exports.getCorrectAnswers = async (setId, cardId) => {
+//   const answer = await fetch(`${rootPath}/card/${setId}/${cardId}
+//     /correct_answers.json`, {
+//     method: 'GET',
+//   });
+//   const correctAnswers = await answer.json();
+//   return correctAnswers ? correctAnswers : [];
+// };
+
+// exports.addWrongAnswers = async (setId, cardId, wrongAnswersList) => {
+//   const wrongAnswersObj = {wrong_answers: wrongAnswersList};
+//   await fetch(`${rootPath}/card/${setId}/${cardId}.json`, {
+//     method: 'PATCH',
+//     body: JSON.stringify(wrongAnswersObj),
+//     headers: {'Content-Type': 'application/json'},
+//   });
+// };
+
+// exports.addCorrectAnswers = async (setId, cardId, correctAnswersList) => {
+//   const correctAnswersObj = {correct_answers: correctAnswersList};
+//   await fetch(`${rootPath}/card/${setId}/${cardId}.json`, {
+//     method: 'PATCH',
+//     body: JSON.stringify(correctAnswersObj),
+//     headers: {'Content-Type': 'application/json'},
+//   });
+// };
+// ------------------------------------------------------------
+//
+//
+//
+//
+// ------------------ INGORE THESE ----------------------------
+// function to grab Set details (params/body)
+// exports.setDetails = async (setId) => {
+//   const answer = await fetch(`${rootPath}/set/${setId}.json`,
+//     {method: 'GET'},
+//   );
+//   const set = await answer.json();
+//   return set ? set : null;
+// };
 
 // need to create a new card/{setiid}/{cardid} endpoint/path to
 // get the specific info of one card? otherwise exports.cardDetails doesn't work
 
-// function to grab Card details (params/body)
-// so that we can grab the front and back of a card
-exports.cardDetails = async (setId, cardId) => {
-  // const answer = await fetch(`${rootPath}/card/${setId}/${cardId}.json`,
-  //   {method: 'GET'},
-  // );
-  const answer = await fetch(`${rootPath}/card/${setId}/${cardId}.json`,
-    {method: 'GET'},
-  );
-  const card = await answer.json();
-  return card ? card : null;
-};
+// // Function to grab Card details (params/body)
+// // so that we can grab the front and back of a card
+//
+// exports.cardDetails = async (setId, cardId) => {
+//   // const answer = await fetch(`${rootPath}/card/${setId}/${cardId}.json`,
+//   //   {method: 'GET'},
+//   // );
+//   const answer = await fetch(`${rootPath}/card/${setId}/${cardId}.json`,
+//     {method: 'GET'},
+//   );
+//   const card = await answer.json();
+//   return card ? card : null;
+// };
 // -------------------------------------------------------------
--- backend/src/llm.js
+++ backend/src/llm.js
@@ -1,4 +1,4 @@
-// const db = require('./db');
+const db = require('./db');
 const OpenAI = require('openai');
 
 // https://openrouter.ai/meta-llama/llama-3.2-1b-instruct:free/api
@@ -11,10 +11,19 @@ exports.llm_test = async (req, res) => {
   const setName = 'CSE 115A: Test 1';
   const front = 'time-boxed';
   const back = 'it ends when time is up, whether or not goal is reached';
-  const sendContent = `In a test about ${setName}, a multiple choice` +
-    ` question "${front}" has the correct answer "${back}". Generate 9` +
-    ' unique incorrect answers in a similar style to the correct answer' +
-    ' in only JSON.';
+  // const sendContent = `In a test about ${setName}, a multiple choice` +
+  //   ` question "${front}" has the correct answer "${back}". Generate 9` +
+  //   ' unique incorrect answers in a similar style to the correct answer' +
+  //   ' in only JSON.';
+
+  // Adjusted the LLM prompt to avoid instances where it sends the JSON ticks
+  const sendContent = `In a test about ${setName}, the multiple-choice ` +
+    `question "${front}" has the correct answer "${back}".` +
+    `Generate exactly 9 unique incorrect answers similar in style to the ` +
+    `correct answer, and respond with only the JSON object without any ` +
+    `surrounding text, quotes, or backticks.`; // The format should be: ` +
+    // `{"incorrect_answers": ["answer1", "answer2", ...]}`;
+
   // console.log(sendContent);
   const completion = await openai.chat.completions.create({
     model: 'meta-llama/llama-3.2-1b-instruct:free',
@@ -25,12 +34,11 @@ exports.llm_test = async (req, res) => {
       },
     ],
   });
-  // console.log(completion.choices[0].message.content);
+  console.log(completion.choices[0].message.content);
 
-  // Parse LLM response into a ist of wrong answers
+  // --- Parse LLM response into a List of answers ---
   const responses = JSON.parse(completion.choices[0].message.content);
   const responsesList = [];
-  // console.log(responses);
 
   for (const r in responses) {
     if (responses.hasOwnProperty(r)) {
@@ -42,11 +50,77 @@ exports.llm_test = async (req, res) => {
     }
   }
 
+  // --- Alternative: Parse LLM output into JSON object ---
+  // const responses = JSON.parse(completion.choices[0].message.content);
+
+  // // create obj to store the array of LLM responses
+  // const responsesObj = {};
+
+  // // prints out a list of objects, each object is one of LLM's wrong answers
+  // for (const i in responses) {
+  //   if (responses.hasOwnProperty(i)) {
+  //     // console.log(responses[i]);
+
+  //     for (const r in responses[i]) {
+  //       if (responses[i][r] != null) {
+  //         responsesObj[r] = responses[i][r];
+  //       } else {
+  //         res.status(500).send('couldnt LLM parse into JSON obj properly');
+  //       }
+  //     }
+  //   }
+  // }
+  // console.log(responsesObj);
+  // ------------------------------------------------------
+
   console.log(responsesList);
+
+  // Test function to add the list of WRONG answers to DB
+  db.addWrongAnswers(front, responsesList);
+  const answer = db.getWrongAnswers();
+  console.log('from the db: ' + answer);
+
+
+  // Test adding CORRECT answers to DB
+  const sendContent2 = `In a test about ${setName}, the multiple-choice ` +
+    `question "${front}" has the correct answer "${back}".` +
+    `Generate exactly 9 unique correct answers similar in style to the ` +
+    `correct answer, and respond with only the JSON object without any ` +
+    `surrounding text, quotes, or backticks.`; // The format should be: ` +
+    // `{"incorrect_answers": ["answer1", "answer2", ...]}`;
+
+  const completion2 = await openai.chat.completions.create({
+    model: 'meta-llama/llama-3.2-1b-instruct:free',
+    messages: [
+      {
+        'role': 'user',
+        'content': sendContent2,
+      },
+    ],
+  });
+  // console.log(completion.choices[0].message.content);
+
+  // Parse LLM response into a List of answers
+  const responses2 = JSON.parse(completion2.choices[0].message.content);
+  const responsesList2 = [];
+
+  for (const r2 in responses2) {
+    if (responses2.hasOwnProperty(r2)) {
+      for (const item2 in responses2[r2]) {
+        if (item2 != null) {
+          responsesList2.push(responses2[r2][item2]);
+        }
+      }
+    }
+  }
+
+  db.addCorrectAnswers(front, responsesList2);
+  console.log('finished adding correct answers to db');
 
   res.status(200).send();
 };
 
+// IGNORE THIS ONE!
 // --- Generates wrong answers for 1 card from a specified set ---
 // exports.llm_generate = async (req, res) => {
 //   // trying to test with a set from mail@email.com account..

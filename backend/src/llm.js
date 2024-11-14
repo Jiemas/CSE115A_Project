9767821a83592cbb5be825c4d8cd0be5a96b74a1
Jiemas <jiemasrubio@yahoo.com>
2024-11-13 21:35:41 -0800

    Changed so requests have both set and card id to work with

--- backend/src/llm.js
+++ backend/src/llm.js
@@ -29,7 +29,7 @@ exports.llm_queue = async (req, res) => {
   let requestsAdded = 0;
   for (card of cards) {
     if (card.llm) continue;
-    await myQueue.add({key: card.key});
+    await myQueue.add({cardId: card.key, setId: setId});
     requestsAdded++;
   }
   res.status(201).json(requestsAdded);

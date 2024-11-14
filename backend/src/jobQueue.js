6de5363ad320529a2db0700f26c274d5b6412c9a
Jiemas <jiemasrubio@yahoo.com>
2024-11-14 11:59:02 -0800

    Some modifications to help with deployment

--- backend/src/jobQueue.js
+++ backend/src/jobQueue.js
@@ -1,6 +1,10 @@
 const Queue = require('bull');
 
 const myQueue = new Queue('myQueueName', {
+  redis: {
+    port: 6379,
+    host: process.env.HOST_NAME,
+  },
   limiter: {
     max: 1,
     duration: 1000,

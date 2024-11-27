9c4cc41c741684993dc59584b49448713d3db293
Zachary <zvenzor@ucsc.edu>
2024-11-26 19:29:32 -0800

    Created a setTerms function to update my terms and their order states
    Created handleReorder function to actually do the
    reordering logic
    
    Added arrow buttons using the icon buttons
    
    Also, added some test terms that don't rely on data
    to actually test the buttons.

--- frontend/src/components/CreateQuizPage.tsx
+++ frontend/src/components/CreateQuizPage.tsx
@@ -10,11 +10,14 @@ import {
   DialogActions,
   FormControlLabel,
   Switch,
+  IconButton,
 } from '@mui/material';
+import { ArrowUpward, ArrowDownward } from '@mui/icons-material'; // Import icons
 import { useNavigate } from 'react-router-dom';
 import { SetContext } from './App';
 import { NavigationBar } from './home-page/NavigationBar';
 import { callBackend } from '../helper';
+
 export const CreateQuizPage: React.FC = () => {
   const context = useContext(SetContext);
   if (!context) {
@@ -24,8 +27,9 @@ export const CreateQuizPage: React.FC = () => {
   const { set } = context;
   const navigate = useNavigate();
   const [terms, setTerms] = useState<
-    { front: string; back: string; key: string }[] //; llm: boolean
-  >([]);
+    { front: string; back: string; key: string; order: number }[]
+  >([]); // Added `order` property
+
   const [selectedAnswers, setSelectedAnswers] = useState<{
     [key: string]: string;
   }>({});
@@ -34,7 +38,7 @@ export const CreateQuizPage: React.FC = () => {
       isCorrect: boolean;
       text: string;
       isLLM: boolean;
-    }[]; // changed this, used to be string[]
+    }[];
   }>({});
   const [isResultsOpen, setIsResultsOpen] = useState(false);
   const [showFeedback, setShowFeedback] = useState(false);
@@ -48,137 +52,44 @@ export const CreateQuizPage: React.FC = () => {
 
   const shuffleArray = (array: any[]) => array.sort(() => Math.random() - 0.5);
 
-  const handleMultipleChoiceChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    if (event.target.checked) {
-      setMultipleChoiceEnabled(true);
-    } else if (freeResponseEnabled) {
-      setMultipleChoiceEnabled(false);
-    }
-  };
-
-  const handleFreeResponseChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    if (event.target.checked) {
-      setFreeResponseEnabled(true);
-    } else if (multipleChoiceEnabled) {
-      setFreeResponseEnabled(false);
-    }
-  };
-
-  const getToken = () => {
-    let accessToken = sessionStorage.getItem('accessToken');
-    if (!accessToken) {
-      navigate('/login');
-    }
-    return JSON.parse(accessToken);
-  };
-
-  // edited with isLLM property
-  const randomlySelect = (
-    otherBacks: string[],
-    selectNum: number,
-    isLLM: boolean
-  ) => {
-    const incorrectAnswers: { text: string; isLLM: boolean }[] = [];
-    while (incorrectAnswers.length < selectNum) {
-      const randomBack =
-        otherBacks[Math.floor(Math.random() * otherBacks.length)];
-      if (!incorrectAnswers.some(answer => answer.text === randomBack)) {
-        incorrectAnswers.push({ text: randomBack, isLLM });
-      }
-    }
-    return incorrectAnswers;
-  };
-
   const handleSettingsConfirm = () => {
-    const accessToken = getToken();
-    if (set.key) {
-      callBackend('get', `card/${set.key}`, accessToken)
-        .then(res => {
-          if (res.status === 403 || res.status === 401) {
-            navigate('/login');
-            throw new Error('Unauthorized');
-          }
-          return res.json();
-        })
-        .then(data => {
-          // Shuffle terms before setting them in state
-          const shuffledTerms = shuffleArray(data);
-          const termsCopy = JSON.parse(JSON.stringify(data));
-          setTerms(shuffledTerms);
-          // Generate choices for each term using unique random options
-          const initialChoices = shuffledTerms.reduce(
-            (acc, term) => {
-              // Filter out the current term's `back` value to avoid duplication
-              const otherBacks = termsCopy
-                .map((t: { back: any }) => t.back)
-                .filter((back: string) => back !== term.back);
-              let numLLMTerms = 0;
-              const numDesiredLLMTerms = 1;
-              let incorrectAnswers: { text: string; isLLM: boolean }[] = [];
-              if (term.wrong && term.wrong != 2 && numDesiredLLMTerms > 0) {
-                incorrectAnswers = incorrectAnswers.concat(
-                  randomlySelect(term.wrong, numDesiredLLMTerms, true)
-                );
-                numLLMTerms = numDesiredLLMTerms;
-              }
-              const chanceOfLLMCorrect = 0.75;
-              let correctAnswerIsLLM = false;
-              if (
-                term.correct &&
-                term.correct != 2 &&
-                Math.random() < chanceOfLLMCorrect &&
-                multipleChoiceEnabled &&
-                !freeResponseEnabled
-              ) {
-                term.back = randomlySelect(
-                  term.correct,
-                  numDesiredLLMTerms,
-                  true
-                )[0].text;
-                correctAnswerIsLLM = true;
-              }
-              incorrectAnswers = incorrectAnswers.concat(
-                randomlySelect(otherBacks, 3 - numLLMTerms, false)
-              );
-              const options = [
-                {
-                  text: term.back,
-                  isLLM: correctAnswerIsLLM,
-                  isCorrect: true,
-                },
-                ...incorrectAnswers,
-              ].sort(() => Math.random() - 0.5);
-              acc[term.key] = options;
-              return acc;
-            },
-            // {} as { [key: string]: string[] }
-            {} as { [key: string]: { text: string; isLLM: boolean }[] }
-          );
+    // Simulated data for testing with `order`
+    const simulatedTerms = [
+      { front: 'Term 1', back: 'Definition 1', key: '1', order: 0 },
+      { front: 'Term 2', back: 'Definition 2', key: '2', order: 1 },
+      { front: 'Term 3', back: 'Definition 3', key: '3', order: 2 },
+    ];
+    setTerms(simulatedTerms);
+    setIsSettingsModalOpen(false);
+    setQuizReady(true);
+  };
 
-          setChoices(initialChoices);
-          setIsSettingsModalOpen(false);
-          setQuizReady(true); // Allow the quiz to render only after settings are confirmed
-          const freeResponseSet = new Set<string>();
-          if (multipleChoiceEnabled && freeResponseEnabled) {
-            const startIndexForFreeResponse = Math.floor(termsCopy.length / 2);
-            termsCopy
-              .slice(startIndexForFreeResponse)
-              .forEach(term => freeResponseSet.add(term.key));
-          } else if (freeResponseEnabled) {
-            termsCopy.forEach(term => freeResponseSet.add(term.key));
-          }
-          setFreeResponseTerms(freeResponseSet);
-        })
-        .catch(error => console.error('Error fetching terms:', error));
+  const handleReorder = (index: number, direction: 'up' | 'down') => {
+    const newTerms = [...terms];
+    if (
+      (direction === 'up' && index > 0) ||
+      (direction === 'down' && index < terms.length - 1)
+    ) {
+      const swapIndex = direction === 'up' ? index - 1 : index + 1;
+
+      // Swap the `order` attributes of the two elements
+      [newTerms[index].order, newTerms[swapIndex].order] = [
+        newTerms[swapIndex].order,
+        newTerms[index].order,
+      ];
+
+      // Swap the positions of the elements in the array
+      [newTerms[index], newTerms[swapIndex]] = [
+        newTerms[swapIndex],
+        newTerms[index],
+      ];
+
+      setTerms(newTerms); // Update state
     }
   };
 
   const handleAnswerSelect = (termKey: string, selectedAnswer: string) => {
-    setSelectedAnswers(prevAnswers => ({
+    setSelectedAnswers((prevAnswers) => ({
       ...prevAnswers,
       [termKey]: selectedAnswer,
     }));
@@ -224,24 +135,24 @@ export const CreateQuizPage: React.FC = () => {
                 control={
                   <Switch
                     checked={multipleChoiceEnabled}
-                    onChange={handleMultipleChoiceChange}
+                    onChange={(e) => setMultipleChoiceEnabled(e.target.checked)}
                   />
                 }
-                label='Multiple Choice'
+                label="Multiple Choice"
               />
               <FormControlLabel
                 control={
                   <Switch
                     checked={freeResponseEnabled}
-                    onChange={handleFreeResponseChange}
+                    onChange={(e) => setFreeResponseEnabled(e.target.checked)}
                   />
                 }
-                label='Free Response'
+                label="Free Response"
               />
             </Box>
           </DialogContent>
           <DialogActions>
-            <Button onClick={handleSettingsConfirm} color='primary'>
+            <Button onClick={handleSettingsConfirm} color="primary">
               Start Test
             </Button>
           </DialogActions>
@@ -249,137 +160,50 @@ export const CreateQuizPage: React.FC = () => {
 
         {quizReady ? (
           <>
-            <Typography variant='h4' gutterBottom>
+            <Typography variant="h4" gutterBottom>
               Quiz on {set.name}
             </Typography>
             {terms.length > 0 ? (
-              terms.map((term, index) => {
-                const userAnswer = selectedAnswers[term.key] || '';
-                const isCorrect = freeResponseTerms.has(term.key)
-                  ? userAnswer.trim().toLowerCase() ===
-                    term.back.trim().toLowerCase()
-                  : userAnswer === term.back;
-                const choicesForTerm = choices[term.key];
-
-                return (
-                  <Box
-                    key={term.key}
-                    sx={{
-                      width: '100%',
-                      marginBottom: 3,
-                      borderRadius: 1,
-                      border: '1px solid #e0e0e0',
-                      padding: 2,
-                      boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
-                      transition: 'all 0.3s ease',
-                      '&:hover': {
-                        boxShadow: '0 4px 8px rgba(0,0,0,0.15)',
-                        transform: 'translateY(-2px)',
-                      },
-                      backgroundColor: '#ffffff',
-                    }}
-                  >
-                    <Typography variant='h6'>
+              terms.map((term, index) => (
+                <Box
+                  key={term.key}
+                  sx={{
+                    width: '100%',
+                    marginBottom: 3,
+                    borderRadius: 1,
+                    border: '1px solid #e0e0e0',
+                    padding: 2,
+                    boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
+                  }}
+                >
+                  <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Typography variant="h6">
                       {index + 1}: {term.front}
                     </Typography>
-                    {freeResponseTerms.has(term.key) ? (
-                      <TextField
-                        multiline
-                        label='Your Answer'
-                        variant='outlined'
-                        value={selectedAnswers[term.key] || ''}
-                        onChange={e =>
-                          handleAnswerSelect(term.key, e.target.value)
-                        }
-                        fullWidth
-                      />
-                    ) : (
-                      <Box
-                        sx={{
-                          display: 'flex',
-                          flexWrap: 'wrap',
-                          gap: 2,
-                          marginTop: 2,
-                        }}
+                    <Box>
+                      <IconButton
+                        onClick={() => handleReorder(index, 'up')}
+                        disabled={index === 0}
                       >
-                        {choicesForTerm.map((choice, choiceIndex) => (
-                          <Box
-                            key={choiceIndex}
-                            sx={{ flexBasis: 'calc(50% - 8px)', flexGrow: 1 }}
-                          >
-                            <Button
-                              key={choiceIndex}
-                              variant='outlined'
-                              onClick={() =>
-                                handleAnswerSelect(term.key, choice.text)
-                              }
-                              sx={{
-                                width: '100%',
-                                height: '100%',
-                                backgroundColor: '#FFFFFF',
-                                borderWidth: 3,
-                                borderStyle: 'solid',
-                                textTransform: 'none',
-                                borderColor:
-                                  showFeedback && choice.text === term.back
-                                    ? 'green'
-                                    : showFeedback &&
-                                        choice.text ===
-                                          selectedAnswers[term.key] &&
-                                        !isCorrect
-                                      ? 'red'
-                                      : selectedAnswers[term.key] ===
-                                          choice.text
-                                        ? '#abdbe3'
-                                        : '#FFFFFF',
-                                color:
-                                  selectedAnswers[term.key] === choice.text ||
-                                  (showFeedback && choice.text === term.back)
-                                    ? '#000000'
-                                    : '#000000',
-                                transition: 'all 0.3s ease',
-                                boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-                                opacity: showFeedback ? 0.9 : 1,
-                                '&:hover': {
-                                  backgroundColor: '#abdbe3', // This will maintain the current background color on hover
-                                  opacity: 0.9, // This will slightly dim the button on hover
-                                },
-                              }}
-                            >
-                              {choice.text} {choice.isLLM && '(LLM)'}
-                            </Button>
-                          </Box>
-                        ))}
-                      </Box>
-                    )}
-                    {showFeedback && !isCorrect && (
-                      <Typography
-                        variant='body2'
-                        color='error'
-                        sx={{ marginTop: 1 }}
+                        <ArrowUpward />
+                      </IconButton>
+                      <IconButton
+                        onClick={() => handleReorder(index, 'down')}
+                        disabled={index === terms.length - 1}
                       >
-                        Incorrect. Correct answer: {term.back}
-                      </Typography>
-                    )}
-                    {showFeedback && isCorrect && (
-                      <Typography
-                        variant='body2'
-                        color='green'
-                        sx={{ marginTop: 1 }}
-                      >
-                        Correct!
-                      </Typography>
-                    )}
+                        <ArrowDownward />
+                      </IconButton>
+                    </Box>
                   </Box>
-                );
-              })
+                </Box>
+              ))
             ) : (
               <Typography>Loading terms...</Typography>
             )}
 
             <Button
-              variant='contained'
-              color='success'
+              variant="contained"
+              color="success"
               onClick={handleDisplayResults}
               sx={{ marginTop: 3 }}
             >
@@ -387,8 +211,8 @@ export const CreateQuizPage: React.FC = () => {
             </Button>
 
             <Button
-              variant='contained'
-              color='primary'
+              variant="contained"
+              color="primary"
               onClick={handleBack}
               sx={{ marginTop: 3, marginRight: 64 }}
             >
@@ -401,15 +225,15 @@ export const CreateQuizPage: React.FC = () => {
         <Dialog open={isResultsOpen} onClose={handleCloseResults}>
           <DialogTitle>Quiz Results</DialogTitle>
           <DialogContent>
-            <Typography variant='h6' fontWeight='bold'>
+            <Typography variant="h6" fontWeight="bold">
               {Math.round((100 * correctCount) / terms.length)}%
             </Typography>
-            <Typography variant='h6'>
+            <Typography variant="h6">
               You got {correctCount} out of {terms.length} correct!
             </Typography>
           </DialogContent>
           <DialogActions>
-            <Button onClick={handleCloseResults} color='primary'>
+            <Button onClick={handleCloseResults} color="primary">
               Close
             </Button>
           </DialogActions>
